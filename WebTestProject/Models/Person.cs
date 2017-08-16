8dee758599321578c17fc70ad01539df5b9da077
71500430 <71500430@aluno.faculdadecotemig.br>
2017-08-16 20:22:30 -0300

    add cpf in person

--- WebTestProject/Models/Person.cs
+++ WebTestProject/Models/Person.cs
@@ -7,6 +7,7 @@ namespace WebTestProject.Models
 {
     public class Person
     {
+        public string CPF { get; set; }
         public string Name { get; set; }
         public string SurNAME { get; set; }
         public int old { get; set; }
