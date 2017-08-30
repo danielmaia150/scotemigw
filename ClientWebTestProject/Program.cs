46dcecc3d3363284cd40d49c3f8d9d5fc071fd8c
71500430 <71500430@aluno.faculdadecotemig.br>
2017-08-30 20:30:52 -0300

    add client

---
+++ ClientWebTestProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientWebTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new HttpClient())
+            {
+                var id = 10;
+
+
+                Console.ReadLine();
+                var expectresult = client.GetStringAsync($"http://localhost:8080/v1/persons/{id}/name");
+                //Console.WriteLine("Request in progress ... ");
+                               
+                var result = expectresult.Result;
+                Console.WriteLine(result);
+                Console.ReadLine();
+
+            }
+            
+        }
+    }
+}
